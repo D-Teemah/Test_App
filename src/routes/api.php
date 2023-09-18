a615afa937cadd873a1dcddf659490351db76139
Olusayo Oyedepo <olusayo@uniswitchng.com>
2023-09-18 08:29:40 +0000

    P15 143

--- src/routes/api.php
+++ src/routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1'], function () {
 
-    Route::group(['middleware' => 'validate-api-key','apiLog'], function () {
+    Route::group(['middleware' => ['validate-api-key', 'apiLog']], function () {
         Route::group(['prefix' => 'verify'], function () {
             Route::post('/', 'KYCController@verify');
         });
